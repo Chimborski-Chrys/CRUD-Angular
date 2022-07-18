842aa3e5426143115a5ec35db80243802cbda0b7
Chrystiomar <chrystiomar@hotmail.com>
2022-07-17 22:10:36 -0300

    entendendo pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import { TodoItemComponent } from './pages/todo/todo-item/todo-item.component';
 import { MatListModule } from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { PipesExampleComponent } from './pages/pipes-example/pipes-example.component';
+import { UpperCasePipe } from '@angular/common';
+import { PrintListPipe } from './print-list.pipe';
 
 
 @NgModule({
@@ -25,7 +28,9 @@ import {MatIconModule} from '@angular/material/icon';
     UsersListComponent,
     UserFormComponent,
     TodoListComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    PipesExampleComponent,
+    PrintListPipe
   ],
   imports: [
     BrowserModule,
@@ -41,7 +46,7 @@ import {MatIconModule} from '@angular/material/icon';
     MatButtonModule,
     MatIconModule    
   ],
-  providers: [],
+  providers: [UpperCasePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
